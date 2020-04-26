6acb47ffcb67d60e0f4dc6e4dc8c81a1ee19c27f
qiaoguangqing <2764800087@qq.com>
2020-04-26 19:38:59 +0800

    构建项目

---
+++ my-app/src/pages/index.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class index extends Component {
+    render() {
+        return (
+            <div>
+                <h1>就是电话交换机</h1>
+            </div>
+        )
+    }
+}
