9eac2b8ba3f98c271a9523780375dfcb8cff9540
qiaoguangqing <2764800087@qq.com>
2020-04-26 21:13:08 +0800

    asdasd

--- my-app/src/pages/index.jsx
+++ my-app/src/pages/index.jsx
@@ -7,7 +7,7 @@ const Home = () => {
             <h1>就是电话交换机</h1>
             <h1>有冲突吗</h1>
             <h1>哈哈</h1>
-            <h2>方一鸣</h2>
+            {/* <h2>方一鸣</h2>
             <Form>
                 <Form.Item label="287">
                     <Input type="text" />
@@ -21,7 +21,7 @@ const Home = () => {
                 <Form.Item label="672e">
                     <Input type="text" />
                 </Form.Item>
-            </Form>
+            </Form> */}
         </div>
     )
 };
