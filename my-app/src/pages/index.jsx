1464ec90eae8fc008717ff2e6d1c11f40e46bc3c
qiaoguangqing <2764800087@qq.com>
2020-04-26 21:08:42 +0800

    antd

--- my-app/src/pages/index.jsx
+++ my-app/src/pages/index.jsx
@@ -1,5 +1,5 @@
-import React, {Component} from 'react';
-import {Form, Input} from 'antd';
+import React, { Component } from 'react';
+import { Form, Input } from 'antd';
 
 const Home = () => {
     return (
@@ -7,9 +7,18 @@ const Home = () => {
             <h1>就是电话交换机</h1>
             <h1>有冲突吗</h1>
             <h1>哈哈</h1>
-                <h2>方一鸣</h2>
+            <h2>方一鸣</h2>
             <Form>
-                <Form.Item label="name">
+                <Form.Item label="287">
+                    <Input type="text" />
+                </Form.Item>
+                <Form.Item>
+                    <Form.Item label="name" noStyle>
+                        <Input type="text" />
+                    </Form.Item>
+                    <span>元</span>
+                </Form.Item>
+                <Form.Item label="672e">
                     <Input type="text" />
                 </Form.Item>
             </Form>
