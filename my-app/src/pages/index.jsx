04645581be13a2e2c142de2803ce4817c562e292
FangyimingLode <FangyimingGo@outlook.com>
2020-04-26 20:25:34 +0800

    冲突

--- my-app/src/pages/index.jsx
+++ my-app/src/pages/index.jsx
@@ -5,7 +5,6 @@ export default class index extends Component {
         return (
             <div>
                 <h1>就是电话交换机</h1>
-                    <h1>有冲突吗</h1>
                 <h1>哈哈</h1>
             </div>
         )
