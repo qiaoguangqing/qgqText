4f6ccd36802e686cdd8034aa636a1c12aa80dd00
qiaoguangqing <2764800087@qq.com>
2020-04-26 20:37:31 +0800

    试试webstorm

---
+++ my-app/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+const  App = () => {
+    return (
+        <div>
+            <h1>哈哈哈哈</h1>
+            <Home />
+        </div>
+    )
+}
+
+
+export default App;
